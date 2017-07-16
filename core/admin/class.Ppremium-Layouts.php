cd879bd6b37f1aee82d2eac1a55cd0e23f018817
Ivaylo Zahariev <ivaylo.zahariev@gmx.com>
2017-07-16 21:36:08 +0300

    Updates

--- core/admin/class.Ppremium-Layouts.php
+++ core/admin/class.Ppremium-Layouts.php
@@ -7,7 +7,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) { exit; } # exit if accessed directly
 
-class Ppremium_Layout extends Playouts_Admin_Layout {
+/*class Ppremium_Layout extends Playouts_Admin_Layout {
 
     function init() {
 
@@ -23,4 +23,4 @@ class Ppremium_Layout extends Playouts_Admin_Layout {
         return '[bw_row row_layout="full" background="color" bg_color="#111111" overlay_bg_second="#f5f5f5" overlay_direction="top right" overlay_opacity="50" vertical_alignment="center" enable_static_height="1" static_height="100"][bw_column overlay_bg_second="#f5f5f5" overlay_opacity="50"][bw_image_slider slide_width="50" spacing="25" thumbnail_size="large" adaptive_height="1" free="1" group_slides="2" autoplay="4000" stop_autoplay_hover="1" pagination_enable="1" infinite="1" invert_color="1" attraction="0.025" friction="0.28"][bw_image_slider_item image="%PLAYOUTS_PATH_ASSETS%images/placeholder-1920_1080-white.png"][/bw_image_slider_item][bw_image_slider_item image="%PLAYOUTS_PATH_ASSETS%images/placeholder-1920_1080-white.png"][/bw_image_slider_item][bw_image_slider_item image="%PLAYOUTS_PATH_ASSETS%images/placeholder-1920_1080-white.png"][/bw_image_slider_item][/bw_image_slider][/bw_column][/bw_row]';
     }
 }
-new Ppremium_Layout;
+new Ppremium_Layout;*/
